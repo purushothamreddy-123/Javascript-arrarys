c15cfaa03b5ea173ecc2f76a939617bf1e2b9d44
Purushothamreddy <purushothamreddydevana@gmail.com>
2022-08-17 14:37:13 +0530

    Javascript array

---
+++ array.js
@@ -0,0 +1,14 @@
+// var myArray = ['h', 'e', 'l', 'l', 'o'];
+
+// // first element
+// console.log(myArray[0]);  
+
+// // second element
+// console.log(myArray[1]);
+
+let dailyActivities = ['eat', 'sleep'];
+
+//add an element at the start
+dailyActivities.unshift('work'); 
+
+console.log(dailyActivities);
